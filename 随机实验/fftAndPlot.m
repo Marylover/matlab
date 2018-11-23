8a1fc19b0275038470453cea3755b655ed82da15
Marylover <755107412@qq.com>
2018-11-23 10:59:29 +0800

    随机信号实验1RC滤波

--- 随机实验/fftAndPlot.m
+++ 随机实验/fftAndPlot.m
@@ -5,10 +5,10 @@ fs = 64*F;
 freq=fft(y,N)*2/N;%做离散傅里叶
 freq_d=abs(fftshift(freq));
 w=(-N/2:1:N/2-1)*fs/N; %双边  
-ylim([0,60]);
-set(gca,'YTick', [0 :10 :60]);
 plot(w,freq_d);
 xlabel('频率/Hz');
 ylabel('幅值/V');
+ylim([0,60]);
+set(gca,'YTick', [0 :10 :60]);
 end
 
